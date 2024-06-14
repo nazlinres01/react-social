089aba708cf7c3c04b3d9ae1c6ed7df9ed440e29
Nazlı Nur ESMERAY <nazlinuresmeray049@gmail.com>
2024-06-14 19:28:34 +0300

    login added

---
+++ .history/src/components/login/Login_20240614183855.jsx
@@ -0,0 +1,7 @@
+import "./login.css"
+
+export default function Login() {
+    return (
+        <div>Login</div>
+    )
+}
---
+++ .history/src/components/login/Login_20240614184037.jsx
@@ -0,0 +1,14 @@
+import "./login.css"
+
+export default function Login() {
+    return (
+        <div className="login">
+            <div className="loginWrapper">
+                <div className="loginLeft"></div>
+                <div className="loginRight"></div>
+
+            </div>
+
+        </div>
+    )
+}
